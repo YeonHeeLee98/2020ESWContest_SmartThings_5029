827c2d6c4e5e6f3a581d47081e17372e52d756b4
nojaeng <45090121+nojaeng@users.noreply.github.com>
2020-08-24 15:38:55 +0900

    Upload configuration file

---
+++ Web/public/config.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyBu16EtBs7PrWwqhdcDIYhMGuq6RE_STek",
+    authDomain: "eswcontest-smartthings.firebaseapp.com",
+    databaseURL: "https://eswcontest-smartthings.firebaseio.com",
+    projectId: "eswcontest-smartthings",
+    storageBucket: "eswcontest-smartthings.appspot.com",
+    messagingSenderId: "706465862197",
+    appId: "1:706465862197:web:96d8ef7915bce48306577c",
+    measurementId: "G-WV0MTWV5QK"
+  };
+  firebase.initializeApp(config);
+  firebase.analytics();
+  
+  var CLIENT_ID = '802361948759-os3vtmlvl88k3uc63a0cogk5ft472o61.apps.googleusercontent.com';
