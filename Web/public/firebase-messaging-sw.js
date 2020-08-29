477dfa89121d38f74a7997880782a7b157f7be61
nojaeng <45090121+nojaeng@users.noreply.github.com>
2020-08-29 14:22:41 +0900

    changing  firebase API key

--- Web/public/firebase-messaging-sw.js
+++ Web/public/firebase-messaging-sw.js
@@ -1,16 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
-// Initialize Firebase
+// Firebase API key
 var config = {
-    apiKey: "AIzaSyBu16EtBs7PrWwqhdcDIYhMGuq6RE_STek",
-    authDomain: "eswcontest-smartthings.firebaseapp.com",
-    databaseURL: "https://eswcontest-smartthings.firebaseio.com",
-    projectId: "eswcontest-smartthings",
-    storageBucket: "eswcontest-smartthings.appspot.com",
-    messagingSenderId: "706465862197",
-    appId: "1:706465862197:web:96d8ef7915bce48306577c",
-    measurementId: "G-WV0MTWV5QK"
+    apiKey: "**************",
+    authDomain: "**************.firebaseapp.com",
+    databaseURL: "https://**************.firebaseio.com",
+    projectId: "**************",
+    storageBucket: "**************.appspot.com",
+    messagingSenderId: "**************",
+    appId: "**************",
+    measurementId: "**************"
 };
 firebase.initializeApp(config);
 
